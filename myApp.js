3371f41c5510b4c648aadb68b4293e48f8daca93
Carlos Cabi <carlangascamachobrigido@gmail.com>
2023-12-06 23:27:52 -0400

    First fcc lesson done in `myApp.js`

--- myApp.js
+++ myApp.js
@@ -1,5 +1,15 @@
-require('dotenv').config();
+require("dotenv").config();
 
+const mongoose = require("mongoose");
+
+// console.log(process.env.MONGO_URI);
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"));
 
 let Person;
 
