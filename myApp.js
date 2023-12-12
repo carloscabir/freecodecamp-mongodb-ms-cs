523f43ee026632a5bdff088d6fbc0e0827c704ef
Carlos Cabi <carlangascamachobrigido@gmail.com>
2023-12-12 18:26:13 -0500

    Forth fcc lesson done at `myApp.js`

--- myApp.js
+++ myApp.js
@@ -48,7 +48,39 @@ const createAndSavePerson = (done) => {
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  Person.create(
+    [
+      {
+        name: "John",
+        age: 30,
+        favoriteFoods: ["Pizza"],
+        contactEmail: "noemail@gmail.com",
+      },
+      {
+        name: "Carlos",
+        age: 16,
+        favoriteFoods: ["Chicken Nuggets", "Tacos", "Mac n Cheese"],
+        contactEmail: "noemail@gmail.com",
+      },
+      {
+        name: "Abby",
+        age: 17,
+        favoriteFoods: ["Chicken Nuggets", "Steak", "Mac n Cheese"],
+        contactEmail: "noemail@gmail.com",
+      },
+      {
+        name: "Vale",
+        age: 16,
+        favoriteFoods: ["Tacos", "Strawberries"],
+        contactEmail: "noemail@gmail.com",
+      },
+    ],
+    (err, data) => {
+      if (err) return console.log(err);
+      done(null, data);
+      return data;
+    }
+  );
 };
 
 const findPeopleByName = (personName, done) => {
