5ea06dd0c055c3ff463d68d38cfb471551a3ce88
Carlos Cabi <carlangascamachobrigido@gmail.com>
2023-12-15 19:05:31 -0500

    `Third fcc` lesson fixed at `myApp.js`

--- myApp.js
+++ myApp.js
@@ -17,15 +17,6 @@ const personSchema = new mongoose.Schema({
   name: { type: String, required: true },
   age: Number,
   favoriteFoods: [String],
-  contactEmail: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    validate: (value) => {
-      return validator.isEmail(value);
-    },
-  },
   createdAt: { type: Date, default: Date.now },
 });
 
@@ -37,7 +28,6 @@ const createAndSavePerson = (done) => {
       name: "John",
       age: 30,
       favoriteFoods: ["Pizza"],
-      contactEmail: "noemail@gmail.com",
     },
     (err, data) => {
       if (err) return console.log(err);
@@ -47,38 +37,33 @@ const createAndSavePerson = (done) => {
   );
 };
 
-const arrayOfPeople = [
+let arrayOfPeople = [
   {
     name: "John",
     age: 30,
     favoriteFoods: ["Pizza"],
-    contactEmail: "noemail@gmail.com",
   },
   {
     name: "Carlos",
     age: 16,
     favoriteFoods: ["Chicken Nuggets", "Tacos", "Mac n Cheese"],
-    contactEmail: "noemail@gmail.com",
   },
   {
     name: "Abby",
     age: 17,
     favoriteFoods: ["Chicken Nuggets", "Steak", "Mac n Cheese"],
-    contactEmail: "noemail@gmail.com",
   },
   {
     name: "Vale",
     age: 16,
     favoriteFoods: ["Tacos", "Strawberries"],
-    contactEmail: "noemail@gmail.com",
   },
 ];
 
 const createManyPeople = (arrayOfPeople, done) => {
   Person.create(arrayOfPeople, (err, data) => {
-    if (err) return console.log(err);
+    if (err) return done(err);
     done(null, data);
-    return data;
   });
 };
 
