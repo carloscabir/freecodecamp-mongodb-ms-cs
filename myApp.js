75ac8fca6b9a60c0cf79581e47a116cad7cd5e0d
Carlos Cabi <carlangascamachobrigido@gmail.com>
2023-12-22 18:23:40 -0500

    11th fcc lesson done at myApp.js

--- myApp.js
+++ myApp.js
@@ -149,7 +149,18 @@ const removeManyPeople = (done) => {
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  done(null /*, data*/);
+  Person
+    .find({ favoriteFoods: foodToSearch })
+    .sort({ name: 1 })
+    .limit(2)
+    .select({ age: 0 })
+    .exec()
+    .then(data => { 
+      done(null, data)
+      console.log(data)
+      return data
+    })
+    .catch(err => done(null))
 };
 
 /** **Well Done !!**
